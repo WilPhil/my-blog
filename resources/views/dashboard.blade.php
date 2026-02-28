e5f86cadb60bb1eaabd2430b57ed8c7a65fe8cab
Wilsent Philip <wphilip465@gmail.com>
2026-02-28 18:01:59 +0700

    add: posts table components for dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">Posts by {{ Auth::user()->name }}</h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xs sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                <div class="text-gray-900">
+                    {{-- {{ __("You're logged in!") }} --}}
+                    <x-posts.table :posts="$posts" />
                 </div>
             </div>
         </div>
